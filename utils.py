edc6b5ca14a791849a63be65f5032dfc46b47da4
p1erce <pierce92@126.com>
2016-06-24 21:35:20 +0800

    94. inorder traversal. 95&96. unique BST structure and num.

--- utils.py
+++ utils.py
@@ -13,3 +13,24 @@ def print_list(head):
         print tmp.val, '->',
         tmp = tmp.next
     print
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+def print_tree(node):
+    ''' level order '''
+    q = [node]
+    while filter(None, q):
+        q_ = []
+        for n in q:
+            if not n:
+                print '\u2205',
+            else:
+                print n.val,
+                q_.append(n.left)
+                q_.append(n.right)
+        q = q_
+        print
